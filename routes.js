bee47331abe367401dabdb1eeaee788bafe20179
Glitch (stirring-fabulous-chickadee) <none>
2024-06-16 07:44:13 +0000

    🔨👡 Checkpoint ./routes.js:68204224/142

--- routes.js
+++ routes.js
@@ -7,4 +7,4 @@ export const router = express.Router();
 router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
-router.post("/dashboard/addplaylist", dashboardController.addPLaylist);
+router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
