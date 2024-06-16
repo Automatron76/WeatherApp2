e45bd950ba9b8449da994545bca77c1b85489007
Glitch (stirring-fabulous-chickadee) <none>
2024-06-16 07:32:00 +0000

    🎸💄 Checkpoint ./controllers/dashboard-controller.js:68204224/110 ./models/playlist-store.js:68204224/1682

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -3,7 +3,8 @@ import { playlistStore } from "../models/playlist-store.js";
 export const dashboardController = {
   async index(request, response) {
     const viewData = {
-      title: "Playlist 1 Dashboard",
+      title: "Playlist Dashboard",
+      playlists: await playlistStore.getAllPlaylists(),
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
@@ -13,7 +14,8 @@ async addPlaylist(request, response) {
   const newPlaylist = {
     title: request.body.title,
   };
-  console.log("adding playlist ${newPlaylist.title}");
+  console.log(`adding playlist ${newPlaylist.title}`);
+  await playlistStore.addPlaylist(newPlaylist);
   response.redirect("/dashboard");
 },
 };
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -3,27 +3,27 @@ import { initStore } from "../utils/store-utils.js";
 
 const db = initStore("playlists");
 
-export const playlistSTore = {
+export const playlistStore = {
   async getAllPlaylists() {
     await db.read();
     return db.data.playlists;
   },
-  
+
   async addPlaylist(playlist) {
     await db.read();
-    playlist._id= v4();
-    db.data.playlist.push(playlist);
+    playlist._id = v4();
+    db.data.playlists.push(playlist);
     await db.write();
     return playlist;
   },
-  
-  async getPlaylitById(id) {
+
+  async getPlaylistById(id) {
     await db.read();
-    const list = db.data.playlist.find((playlist) => playlist._id ===id);
+    const list = db.data.playlists.find((playlist) => playlist._id === id);
     return list;
   },
-  
-   async deletePlaylistById(id) {
+
+  async deletePlaylistById(id) {
     await db.read();
     const index = db.data.playlists.findIndex((playlist) => playlist._id === id);
     db.data.playlists.splice(index, 1);
