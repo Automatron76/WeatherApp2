057c67bf5ab4eb95e2acf7cf78adbcbb313c1742
Glitch (stirring-fabulous-chickadee) <none>
2024-06-24 08:43:01 +0100

    improved landing page, created  a for loop to get min max values from reports, still working on getting the values displayed

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -4,10 +4,62 @@ import { reportStore } from "../models/report-store.js";
 export const stationController = {
   
   async index(request, response) {
+    
     const station = await stationStore.getStationById(request.params.id);
+    
+    
+let minTempReport = null;
+let maxTempReport = null;
+let minWindSpeedReport = null;
+let maxWindSpeedReport = null;
+let minPressureReport = null;
+let maxPressureReport = null;
+
+if (station.reports.length > 0) {
+    minTempReport = station.reports[0];
+    maxTempReport = station.reports[0];
+    minWindSpeedReport = station.reports[0];
+    maxWindSpeedReport = station.reports[0];
+    minPressureReport = station.reports[0];
+    maxPressureReport = station.reports[0];
+
+    for (let i = 1; i < station.reports.length; i++) {
+        if (station.reports[i].temp < minTempReport.temp) {
+            minTempReport = station.reports[i];
+        }
+        if (station.reports[i].temp > maxTempReport.temp) {
+            maxTempReport = station.reports[i];
+        }
+        if (station.reports[i].windSpeed < minWindSpeedReport.windSpeed) {
+            minWindSpeedReport = station.reports[i];
+        }
+        if (station.reports[i].windSpeed > maxWindSpeedReport.windSpeed) {
+            maxWindSpeedReport = station.reports[i];
+        }
+        if (station.reports[i].pressure < minPressureReport.pressure) {
+            minPressureReport = station.reports[i];
+        }
+        if (station.reports[i].pressure > maxPressureReport.pressure) {
+            maxPressureReport = station.reports[i];
+        }
+    }
+}
+
+console.log({
+    minTemp: minTempReport.temp,
+    maxTemp: maxTempReport.temp,
+    minWindSpeed: minWindSpeedReport.windSpeed,
+    maxWindSpeed: maxWindSpeedReport.windSpeed,
+    minPressure: minPressureReport.pressure,
+    maxPressure: maxPressureReport.pressure
+});
+
+ 
+
     const viewData = {
       title: "Station",
       station: station,
+      minTemp: minTempReport.temp,
     };
     response.render("station-view", viewData);
   },
