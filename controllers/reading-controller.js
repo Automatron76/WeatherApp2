afca80fec1799e87ae7045ad3dae932df8382254
Giovanni <giod92@live.it>
2024-07-09 08:27:56 +0100

    finally managed to use the API data inside the graph to plot temp forecast every 3 hours. Improved typography in the auto generated report. uploaded everything on glitch to test and so far everything works

--- controllers/reading-controller.js
+++ controllers/reading-controller.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { stationStore } from "../models/station-store.js";
+import dayjs from "dayjs";  // Import dayjs
 
 export const readingController = {
 async index(request, response) {
@@ -10,9 +11,11 @@ async index(request, response) {
       const title = station.title;
 
       const weatherRequestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${title}&appid=55f42099079e1f2b191983b4311a383a`
-      const forecastRequestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=55f42099079e1f2b191983b4311a383a`
+      const forecastRequestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&exclude=daily,minutely,current,alerts&units=metric&appid=55f42099079e1f2b191983b4311a383a`
       const apiResponse = await axios.get(weatherRequestUrl);
       const forecastResponse = await axios.get(forecastRequestUrl);
+
+      const dateTime = dayjs().format("dddd, MMM D, YYYY h:mm A")
 
       const weatherData = apiResponse.data;
       const forecastData = forecastResponse.data;
@@ -22,13 +25,14 @@ async index(request, response) {
       }
 
        // Extract temperature values from the forecast data
-       const temperatures = forecastData.list.map(item => parseFloat((item.main.temp - 273.15).toFixed(2))); // Convert Kelvin to Celsius
+       const temperatures = forecastData.list.map(item => parseFloat((item.main.temp))); 
            // Log the extracted temperatures for debugging
-      console.log( temperatures);
+      console.log(lat, lon, station);
 
         const viewData = {
           title: "Auto Report",
           weather: {
+            dateTime: dateTime,  
             stationName: weatherData.name,
             temp: (weatherData.main.temp - 273.15).toFixed(2), // Convert Kelvin to Celsius
             feelsLike: (weatherData.main.feels_like - 273.15).toFixed(2), // Convert Kelvin to Celsius
@@ -39,12 +43,13 @@ async index(request, response) {
             windSpeed: weatherData.wind.speed,
             windDegree: weatherData.wind.deg,
             description: weatherData.weather[0].description,
-            icon: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
+            icon: `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`,
           },
           chartData: {
           temperatures:temperatures
         }
-          
+       
+
         };
 
         response.render("autoreport-view", viewData);
